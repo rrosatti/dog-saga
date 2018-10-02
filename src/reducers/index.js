632df31379a6232755eaa2b5f65ac779b646612a
Rodrigo R. Galvao <rodrigorosattig@gmail.com>
2018-10-02 15:24:21 -0300

    Implementation of: reducers, actions and sagas
    A simple implementation of reducers, actions and sagas were made,
    and put in their respective directories.
    
    Signed-off-by: Rodrigo R. Galvao <rodrigorosattig@gmail.com>

---
+++ src/reducers/index.js
@@ -0,0 +1,22 @@
+import * as types from '../actions';
+
+const initiallState = {
+  fetching: false,
+  dog: null,
+  error: null
+};
+
+const reducer = (state = initiallState, action) => {
+  switch (action.type) {
+    case types.API_CALL_REQUEST:
+      return { ...state, fetching: true, error: null };
+    case types.API_CALL_SUCCESS:
+      return { ...state, fetching: false, dog: action.dog };
+    case types.API_CALL_FAILURE:
+      return { ...state, fetching:false, dog: null, error: action.error };
+    default:
+      return state;
+  }
+}
+
+export default reducer;
